7303efecea4b087dce2252f9f0804f7e29c4dd6e
dongmin132 <m914@naver.com>
2024-08-07 00:32:36 +0900

    feat: Proxy 설정

--- src/main/java/org/example/camping/CampingApplication.java
+++ src/main/java/org/example/camping/CampingApplication.java
@@ -1,7 +1,15 @@
 package org.example.camping;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.Proxy;
+import java.net.InetSocketAddress;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CampingApplication {
@@ -9,5 +17,29 @@ public class CampingApplication {
     public static void main(String[] args) {
         SpringApplication.run(CampingApplication.class, args);
     }
+    @Bean
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
 
+        // 프록시 설정
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("krmp-proxy.9rum.cc", 3128));
+        requestFactory.setProxy(proxy);
+
+        return new RestTemplate(requestFactory);
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) {
+        return args -> {
+            try {
+                String url = "https://api.github.com/users";
+                String[] response = restTemplate.getForObject(url, String[].class);
+
+                // 응답 결과 출력
+                Arrays.stream(response).forEach(System.out::println);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+    }
 }
