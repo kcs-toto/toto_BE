2b27a1f84f077d283a899709b2b338cebae2224b
dongmin132 <m914@naver.com>
2024-08-06 10:21:49 +0900

    feat: 전역 설정 추가

---
+++ src/main/java/org/example/camping/global/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package org.example.camping.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    // 서버 오류
+    SEVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SV001", "서버 오류");
+
+    private final HttpStatus httpStatus;
+
+    private final String code;
+
+    private final String message;
+}
