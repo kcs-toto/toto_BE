d253f98fa0eb16606d6d77599070b78a23a7a125
dongmin132 <m914@naver.com>
2024-08-07 00:29:05 +0900

    fix: cors 설정 오류 수정

--- src/main/java/org/example/camping/global/config/WebConfig.java
+++ src/main/java/org/example/camping/global/config/WebConfig.java
@@ -7,11 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 // Spring 서버 전역적으로 CORS 설정
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 허용할 출처 : 특정 도메인만 받을 수 있음
-                .allowedMethods("GET", "POST") // 허용할 HTTP method
-                .allowCredentials(true); // 쿠키 인증 요청 허용
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .maxAge(3600L)
+                .allowCredentials(true);
     }
 }
